ee1192af5772b02a67b3e8c9210beac09b904254
dimitristm <115984529+dimitristm@users.noreply.github.com>
2026-01-27 22:11:54 +0200

    basic renderer

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(CMAKE_C_FLAGS_INIT "-fuse-ld=mold")
-set(CMAKE_CXX_FLAGS_INIT "-fuse-ld=mold")
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "-fuse-ld=mold")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=mold")
